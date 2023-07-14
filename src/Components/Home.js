4a60e06c4f528a308a49b412f4a9ed8b81500ad2
PetejStone <petejstoney@gmail.com>
2023-07-14 13:44:56 -0500

    Firt Commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -24,7 +24,7 @@ const Home = () =>  {
        setGuess(letter);
 
          //add guessed letter to guessedLetters Array
-         for (let i=0; i < lettersGuessed.length; i++) {
+         for (let i=0; i <= lettersGuessed.length; i++) {
            if (e === lettersGuessed[i]) {
                 console.log('guess',e,lettersGuessed[i])
                alert( `You've already guessed that letter, pick another ones!` );
@@ -44,19 +44,7 @@ const Home = () =>  {
        for (let i = 0; i < originalWord.length; i++) {
             
           
-           if (!word.includes(e.toLowerCase())) {
-              //decrease guesses by one
-               
-           //     let wrong = lives - 1;
-               
-             
-           //     //If lives are less than or equal to one, alert they lost
-           //     if (lives <= 1) {
-           //         console.log('YOU LOST')
-           //         return alert(`You Lost, the word was "${word}" `)
-           //     }
-           //    setLives(wrong)
-           }
+        
 
            //if letter exists, show on front end
            if (originalWord[i] === e.toLowerCase())  {
@@ -129,138 +117,134 @@ const Home = () =>  {
           setLives(wrong)
            
 
-           // setTimeout(()=> {
-           //     // console.log('timeout',head)
-           //    let head = document.querySelector('.headParent')
-           //     head.classList.remove('zoom')
-           // },1000)
+         
        }
     }
 
 
-    const handleKeyPress = e => {
+//     const handleKeyPress = e => {
        
            
         
 
-        // Set input to the letter pressed
-        let letter = e.target.value.toLowerCase();
-        console.log('letter',e.target.value)
-        setGuess(letter);
+//         // Set input to the letter pressed
+//         let letter = e.target.value.toLowerCase();
+//         console.log('letter',e.target.value)
+//         setGuess(letter);
 
-          //add guessed letter to guessedLetters Array
-          for (let i=0; i < lettersGuessed.length; i++) {
-            if (e.target.value.toLowerCase() === lettersGuessed[i]) {
-                alert( `You've already guessed that letter, pick another one!` );
-                setGuess('')
-                return
-            }
-          }
-          let updateGuessed = [...lettersGuessed];
-          if (!word.includes(e.target.value.toLowerCase())) updateGuessed.push(e.target.value.toLowerCase());
-          setLettersGuessed(updateGuessed)
+//           //add guessed letter to guessedLetters Array
+//           for (let i=0; i < lettersGuessed.length; i++) {
+//             if (e.target.value.toLowerCase() === lettersGuessed[i]) {
+//                 alert( `You've already guessed that letter, pick another one!` );
+//                 setGuess('')
+//                 return
+//             }
+//           }
+//           let updateGuessed = [...lettersGuessed];
+//           if (!word.includes(e.target.value.toLowerCase())) updateGuessed.push(e.target.value.toLowerCase());
+//           setLettersGuessed(updateGuessed)
           
           
 
 
-        //if letter doesn't exist in word
-        let exists = correct
-        for (let i = 0; i < originalWord.length; i++) {
+//         //if letter doesn't exist in word
+//         let exists = correct
+//         for (let i = 0; i < originalWord.length; i++) {
              
            
-            if (!word.includes(e.target.value.toLowerCase())) {
-               //decrease guesses by one
+//             if (!word.includes(e.target.value.toLowerCase())) {
+//                //decrease guesses by one
                 
-            //     let wrong = lives - 1;
+//             //     let wrong = lives - 1;
                 
               
-            //     //If lives are less than or equal to one, alert they lost
-            //     if (lives <= 1) {
-            //         console.log('YOU LOST')
-            //         return alert(`You Lost, the word was "${word}" `)
-            //     }
-            //    setLives(wrong)
-            }
+//             //     //If lives are less than or equal to one, alert they lost
+//             //     if (lives <= 1) {
+//             //         console.log('YOU LOST')
+//             //         return alert(`You Lost, the word was "${word}" `)
+//             //     }
+//             //    setLives(wrong)
+//             }
 
-            //if letter exists, show on front end
-            if (originalWord[i] === e.target.value.toLowerCase())  {
-                blank[i] = e.target.value.toLowerCase();
+//             //if letter exists, show on front end
+//             if (originalWord[i] === e.target.value.toLowerCase())  {
+//                 blank[i] = e.target.value.toLowerCase();
             
-                let update = [...blank];
-                setBlank(update);
-                exists = exists += 1
+//                 let update = [...blank];
+//                 setBlank(update);
+//                 exists = exists += 1
                 
                
-                setGuess('')
+//                 setGuess('')
                
-                if (correct === word.length -1) {
-                    setTimeout(() => {
-                        alert('Congratulations! You Won!')
+//                 if (correct === word.length -1) {
+//                     setTimeout(() => {
+//                         alert('Congratulations! You Won!')
 
-                    },200)
+//                     },200)
                    
-                }
-            } 
+//                 }
+//             } 
 
             
-        }
-        setCorrect(exists)
-        console.log(exists, word.length)
-        //reset guess to blank
-        setGuess('')
+//         }
+//         setCorrect(exists)
+//         console.log(exists, word.length)
+//         //reset guess to blank
+//         setGuess('')
 
-        if (word.includes(e.target.value.toLowerCase())) {
-            console.log('true');
-        } else {
-            console.log('zoom in body part', lives);
-            let wrapperParent = document.querySelector('.wrapperParent')
-            wrapperParent.classList.add('active')
+//         if (word.includes(e.target.value.toLowerCase())) {
+//             console.log('true');
+//         } else {
+//             console.log('zoom in body part', lives);
+//             let wrapperParent = document.querySelector('.wrapperParent')
+//             wrapperParent.classList.add('active')
 
-            if (lives === 7) {
-                let rope = document.querySelector('.ropeParent')
-                rope.classList.add('zoom')
-            } else if (lives === 6) {
-                let head = document.querySelector('.headParent')
-                head.classList.add('zoom')
-            } else if (lives === 5) {
-                let torso = document.querySelector('.torsoParent')
-                torso.classList.add('zoom')
-            } else if (lives === 4) {
-                let rightarm = document.querySelector('.rightarmParent')
-                rightarm.classList.add('zoom')
-            } else if (lives === 3) {
-                let leftarm = document.querySelector('.leftarmParent')
-                leftarm.classList.add('zoom')
-            } else if (lives === 2) {
-                let rightleg = document.querySelector('.rightlegParent')
-                rightleg.classList.add('zoom')
-            } else if (lives === 1) {
-                let leftleg = document.querySelector('.leftlegParent')
-                leftleg.classList.add('zoom')
-            }
+//             if (lives === 7) {
+//                 let rope = document.querySelector('.ropeParent')
+//                 rope.classList.add('zoom')
+//             } else if (lives === 6) {
+//                 let head = document.querySelector('.headParent')
+//                 head.classList.add('zoom')
+//             } else if (lives === 5) {
+//                 let torso = document.querySelector('.torsoParent')
+//                 torso.classList.add('zoom')
+//             } else if (lives === 4) {
+//                 let rightarm = document.querySelector('.rightarmParent')
+//                 rightarm.classList.add('zoom')
+//             } else if (lives === 3) {
+//                 let leftarm = document.querySelector('.leftarmParent')
+//                 leftarm.classList.add('zoom')
+//             } else if (lives === 2) {
+//                 let rightleg = document.querySelector('.rightlegParent')
+//                 rightleg.classList.add('zoom')
+//             } else if (lives === 1) {
+//                 let leftleg = document.querySelector('.leftlegParent')
+//                 leftleg.classList.add('zoom')
+//             }
 
-            let wrong = lives - 1;
+//             let wrong = lives - 1;
                 
               
-            //If lives are less than or equal to one, alert they lost
-            if (lives <= 1) {
-                console.log('YOU LOST')
-                setTimeout(() => {
-                    alert(`You Lost, the word was "${word}" `)
-                },2000)
+//             //If lives are less than or equal to one, alert they lost
+//             if (lives <= 1) {
+//                 console.log('YOU LOST')
+//                 setTimeout(() => {
+//                     alert(`You Lost, the word was "${word}" `)
+//                 },2000)
                 
-            }
-           setLives(wrong)
+//             }
+//            setLives(wrong)
             
 
-            // setTimeout(()=> {
-            //     // console.log('timeout',head)
-            //    let head = document.querySelector('.headParent')
-            //     head.classList.remove('zoom')
-            // },1000)
-        }
+//             // setTimeout(()=> {
+//             //     // console.log('timeout',head)
+//             //    let head = document.querySelector('.headParent')
+//             //     head.classList.remove('zoom')
+//             // },1000)
+//         }
         
-   }
+//    }
  
 
     const newWord = e => {
